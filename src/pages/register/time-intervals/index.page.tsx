b9182198db6056b10d12fef7b6848a2a6e259651
Andre Luiz <adventureandre@hotmail.com>
2024-01-09 22:27:51 -0300

    Configurando disponibilidade - Página de disponibilidade

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,51 @@
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@advfw-ui/react'
+import { Container, Header } from '../styles'
+import {
+  IntervalBox,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+  IntervalsContainer,
+} from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá</Heading>
+        <Text>
+          Defina o intervalo de horários que você está disponivel em cada dia da
+          semana.
+        </Text>
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+
+      <IntervalBox as="form">
+        <IntervalsContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Segunda-feria</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+        </IntervalsContainer>
+
+        <Button type="submit">
+          Proximo passo <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  )
+}
