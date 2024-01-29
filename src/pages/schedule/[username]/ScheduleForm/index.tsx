26354f20b28cd613486d7146820e75ef4c1f4c33
Andre Luiz <adventureandre@hotmail.com>
2024-01-28 21:02:33 -0300

    Página de confirmação

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,5 +1,5 @@
-import { CalendarStep } from '@/pages/schedule/[username]/ScheduleForm/CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  return <ConfirmStep />
 }
